d8b00286c8db684c3650cf6e9a6b68764e03c9ef
Andrey <andrtaw1@gmail.com>
2019-10-05 18:19:25 +0300

    feat: add basic config, styles and packages

--- src/renderer/nuxt.config.js
+++ src/renderer/nuxt.config.js
@@ -7,13 +7,57 @@
 module.exports = {
   mode: 'spa', // or 'universal'
   head: {
-    title: 'nknx-vault'
+    title: 'nknx-vault',
+    link: [
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Karla:400,400i,700,700i|Rubik:300,400,500,700,900&display=swap'
+      }
+    ]
   },
-  loading: false,
-  plugins: [
-
+  /*
+   ** Global CSS
+   */
+  css: [
+    '~/assets/css/reset.css',
+    '~/assets/css/feather.css',
+    '@/assets/scss/main.scss'
   ],
+  loading: false,
+  plugins: [],
   modules: [
-
-  ]
+    '@nuxtjs/style-resources',
+    'nuxt-svg-loader',
+    '@nuxtjs/axios',
+    [
+      '@nuxtjs/moment',
+      {
+        locales: [
+          'de',
+          'ru',
+          'fr',
+          'tr',
+          'es',
+          'nl',
+          'hu',
+          'ja',
+          'fi',
+          'vi',
+          'sk',
+          'zh-cn'
+        ],
+        defaultLocale: 'en'
+      }
+    ]
+  ],
+  styleResources: {
+    scss: [
+      '~/assets/scss/_app.variables.scss',
+      '~/assets/scss/_app.mixins.scss'
+    ]
+  },
+  axios: {
+    baseURL: 'https://api.nknx.nkn.org/'
+  }
 }
