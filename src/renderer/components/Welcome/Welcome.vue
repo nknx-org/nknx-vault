bf0ab3c17006f25195635462e351e436312d2484
Christian Busch <kontakt@christian-busch.com>
2019-10-14 15:32:58 +0200

    feat: esLint fixes

--- src/renderer/components/Welcome/Welcome.vue
+++ src/renderer/components/Welcome/Welcome.vue
@@ -4,15 +4,17 @@
       <div class="welcome__content">
         <div class="welcome__left">
           <back class="welcome__back" />
-          <h2 class="title_light">{{$t('welcomeHeading')}}</h2>
-          <p class="welcome__descr">{{$t('welcomeDescription')}}</p>
+          <h2 class="title_light">{{ $t('welcomeHeading') }}</h2>
+          <p class="welcome__descr">{{ $t('welcomeDescription') }}</p>
           <div class="welcome__controls">
             <Button
               class="welcome__btn"
               type="router"
               url="createWallet"
               theme="success"
-            >{{ $t('createWallet') }}</Button>
+            >
+              {{ $t('createWallet') }}
+            </Button>
             <Button class="welcome__btn" theme="ghost">{{ $t('import') }}</Button>
           </div>
         </div>
@@ -20,7 +22,7 @@
       </div>
       <div class="welcome__disclaimer">
         <Fragment class="welcome__disclaimer-fragment" />
-        <p class="welcome__disclaimer-text">{{$t('welcomeDisclaimer')}}</p>
+        <p class="welcome__disclaimer-text">{{ $t('welcomeDisclaimer') }}</p>
       </div>
     </div>
   </div>
@@ -31,16 +33,16 @@
 </style>
 
 <script>
-import Back from "~/assets/icons/logo-back.svg";
-import Share from "~/assets/icons/share.svg";
-import Fragment from "~/assets/icons/welcome-fragment.svg";
-import Button from "~/components/Button/Button.vue";
+import Back from '~/assets/icons/logo-back.svg'
+import Share from '~/assets/icons/share.svg'
+import Fragment from '~/assets/icons/welcome-fragment.svg'
+import Button from '~/components/Button/Button.vue'
 
 export default {
   components: { Back, Share, Button, Fragment },
-  data() {
-    return {};
+  data () {
+    return {}
   },
   methods: {}
-};
+}
 </script>
