11d58c8f38c9ecf9b0b2e995a653bb47e3023897
Andrey <andrtaw1@gmail.com>
2019-10-19 20:23:25 +0300

    feat: add backup

--- src/renderer/components/Settings/Settings.vue
+++ src/renderer/components/Settings/Settings.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="settings">
-    <div class="settings__item">
+    <!-- <div class="settings__item">
       <div class="settings__left">
         <h3 class="title_color_dark">NKN {{ $t('nameService') }}</h3>
         <p class="settings__descr"><span v-if="walletName.length > 0">{{ $t('registeredWalletDescr') }}</span> <span v-else>{{ $t('unregisteredWalletDescr') }}</span> </p>
       </div>
       <div class="settings__right"><Button theme="ghost-secondary" @click.native="walletName.length > 0 ? openDeleteWalletNameModal : toggleRegisterWalletNameModal(true)"><span v-if="walletName.length > 0">{{ $t('deleteName') }}</span> <span v-else>{{ $t('registerName') }}</span></Button></div>
-    </div>
+    </div> -->
     <RegisterNameModal :open="isRegisterWalletNameModal" @toggleRegisterWalletNameModal="toggleRegisterWalletNameModal" />
+    <div class="settings__item">
+      <div class="settings__left">
+        <h3 class="title_color_dark">{{ $t('backup') }}</h3>
+        <p class="settings__descr"> {{ $t('backupDescr') }}</p>
+      </div>
+      <div class="settings__right"><Button theme="ghost-secondary" :click="downloadWallet"> {{ $t('download') }}</Button></div>
+    </div>
   </div>
 </template>
 
@@ -16,7 +23,10 @@
 </style>
 
 <script>
+import FileSaver from 'file-saver'
+
 import { mapGetters } from 'vuex'
+
 import Button from '~/components/Button/Button.vue'
 import RegisterNameModal from '~/components/Modals/RegisterNameModal/RegisterNameModal.vue'
 
@@ -39,6 +49,11 @@ export default {
   methods: {
     toggleRegisterWalletNameModal (bool) {
       this.isRegisterWalletNameModal = bool
+    },
+    downloadWallet () {
+      const jsonWallet = this.activeWallet.toJSON()
+      const blob = new Blob([jsonWallet], { type: 'text/plain;charset=utf-8' })
+      FileSaver.saveAs(blob, 'wallet.json')
     }
   }
 }
