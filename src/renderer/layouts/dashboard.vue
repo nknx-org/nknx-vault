94ea790ecf436484da0d8f5cfccabf39ab97f795
Andrey <andrtaw1@gmail.com>
2019-10-21 19:38:11 +0300

    feat: add offline mode alert

--- src/renderer/layouts/dashboard.vue
+++ src/renderer/layouts/dashboard.vue
@@ -8,14 +8,29 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 import Snackbar from '~/components/Snackbar/Snackbar.vue'
 import Sidebar from '~/components/Sidebar/Sidebar.vue'
 import WalletPanel from '~/components/WalletPanel/WalletPanel.vue'
 
 export default {
   components: { Snackbar, Sidebar, WalletPanel },
+  computed: {
+    ...mapGetters({
+      online: 'online/getOnline'
+    })
+  },
   created () {
     this.$store.dispatch('online/updateOnline')
+  },
+  mounted () {
+    if (this.online === false) {
+      this.$store.dispatch('snackbar/updateSnack', {
+        snack: 'offlineModeAlert',
+        color: 'alert'
+      })
+    }
   }
 }
 </script>
