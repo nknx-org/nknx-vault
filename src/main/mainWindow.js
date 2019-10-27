a2876d5b4252f5faaf3ef623891b7226c05438f6
Christian Busch <kontakt@christian-busch.com>
2019-10-27 15:23:49 +0100

    feat: removed max width

--- src/main/mainWindow.js
+++ src/main/mainWindow.js
@@ -9,9 +9,8 @@ const winHandler = new BrowserWinHandler({
   height: 855,
   minHeight: 855,
   maxHeight: 855,
-  width: 1201,
+  width: 1080,
   minWidth: 1080,
-  maxWidth: 1381,
   transparent: true,
   frame: false
 })
