f57bd935aac4d326b58af0af11daf5113ae9dbb9
Andrey <andrtaw1@gmail.com>
2020-05-06 13:32:45 +0300

    fix: transfer validation

--- src/renderer/components/Transfer/Transfer.vue
+++ src/renderer/components/Transfer/Transfer.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="transfer">
-    <ConfirmTransferModal :open="isTransferConfirmModal" :fee="fee !== '' ? parseFloat(fee) : 0" :address="address" :amount="parseFloat(amount)" @toggleTransferConfirmModal="toggleTransferConfirmModal" />
+    <ConfirmTransferModal
+      :open="isTransferConfirmModal"
+      :fee="fee !== '' ? parseFloat(fee) : 0"
+      :address="address"
+      :amount="parseFloat(amount)"
+      @toggleTransferConfirmModal="toggleTransferConfirmModal"
+    />
 
     <div class="transfer__controls">
       <label class="modal__label transfer__amount">
-        {{ $t('amount') }} <span
+        {{ $t("amount") }}
+        <span
           v-tooltip="{
             content: $t('sendAmountTooltip'),
             placement: 'top-center',
-            offset: 5,
+            offset: 5
           }"
           class="transfer__info fe fe-info"
         />
@@ -16,7 +23,9 @@
           <input
             v-model="amount"
             class="modal__controller"
-            :class="!isAmount && isAmountChange ? 'modal__controller_error' : null"
+            :class="
+              !isAmount && isAmountChange ? 'modal__controller_error' : null
+            "
             type="number"
             @keydown="numFilter"
           >
@@ -24,11 +33,12 @@
       </label>
       <TxIcon class="transfer__icon" />
       <label class="modal__label transfer__address">
-        {{ $t('toWalletAddress') }} <span
+        {{ $t("toWalletAddress") }}
+        <span
           v-tooltip="{
             content: $t('toWalletAddressTooltip'),
             placement: 'top-center',
-            offset: 5,
+            offset: 5
           }"
           class="transfer__info fe fe-info"
         />
@@ -36,7 +46,9 @@
           <input
             v-model.trim="address"
             class="modal__controller"
-            :class="!isAddress && isAddressChange ? 'modal__controller_error' : null"
+            :class="
+              !isAddress && isAddressChange ? 'modal__controller_error' : null
+            "
             type="text"
           >
         </div>
@@ -45,16 +57,21 @@
 
     <div class="transfer__controls">
       <label class="modal__label transfer__amount">
-        {{ $t('networkFees') }} <span
+        {{ $t("networkFees") }}
+        <span
           v-tooltip="{
             content: $t('networkFeesTooltip'),
             placement: 'top-center',
-            offset: 5,
+            offset: 5
           }"
           class="transfer__info fe fe-info"
         />
-        <Select type="modal" :items="feesTypes" :active-item="activeFeesType" @update="updateFeesType" />
-
+        <Select
+          type="modal"
+          :items="feesTypes"
+          :active-item="activeFeesType"
+          @update="updateFeesType"
+        />
       </label>
 
       <label class="modal__label transfer__fee">
@@ -75,7 +92,7 @@
         :disabled="!isOk"
         @click.native="isOk ? toggleTransferConfirmModal(true) : false"
       >
-        {{ $t('send') }}
+        {{ $t("send") }}
       </Button>
     </div>
   </div>
@@ -126,23 +143,42 @@ export default {
       return this.address.length > 0
     },
     isAmount () {
-      return this.amount > 0 && this.amount < parseFloat(this.walletInfo.balance) && this.checkNumLimit(this.amount)
+      return (
+        this.amount > 0 &&
+        this.amount < parseFloat(this.walletInfo.balance) &&
+        this.checkNumLimit(this.amount)
+      )
     },
     isAmountChange () {
       return this.amount !== ''
     },
     isFee () {
-      return this.fee >= 0 && parseFloat(this.walletInfo.balance) >= this.fee + this.amount && this.checkNumLimit(this.fee)
+      return (
+        this.fee >= 0 &&
+        parseFloat(this.walletInfo.balance) >=
+          Number(this.fee) + Number(this.amount) &&
+        this.checkNumLimit(this.fee)
+      )
     },
     isFeeChange () {
       return this.fee !== 0.5
     },
     isOk () {
-      return this.isAmount === true && this.isAddress === true && this.isFee === true
+      return (
+        this.isAmount === true && this.isAddress === true && this.isFee === true
+      )
     },
     activeFeesType () {
       const fee = this.fee
-      return fee === this.highFee ? 'high' : fee === this.standardFee ? 'standard' : fee === this.lowFee ? 'low' : fee === this.minimalFee ? 'minimal' : 'custom'
+      return fee === this.highFee
+        ? 'high'
+        : fee === this.standardFee
+          ? 'standard'
+          : fee === this.lowFee
+            ? 'low'
+            : fee === this.minimalFee
+              ? 'minimal'
+              : 'custom'
     }
   },
   created () {
@@ -167,7 +203,12 @@ export default {
       return parseFloat(fee).toFixed(8)
     },
     checkNumLimit (x) {
-      const limit = ((x.toString().includes('.')) ? (x.toString().split('.').pop().length) : (0))
+      const limit = x.toString().includes('.')
+        ? x
+          .toString()
+          .split('.')
+          .pop().length
+        : 0
       if (limit > 8) {
         return false
       } else {
